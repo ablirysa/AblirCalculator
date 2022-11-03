3331a88940a245c26977c0d99a49077d2bc95c3c
ablirysa <ysabellea@nycstudents.net>
2022-11-02 20:31:33 -0400

    finished

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class Calculator {
+public class Calculator {
+    // instance variables
+    private String exp;
+    private double num1;
+    private double num2;
+
+    // constructor
+    public Calculator (String exp, double num1, double num2) {
+        this.exp = exp;
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    // determine which equation
+    public double getAnswer() {
+        // finds operation in expression
+        int add = exp.indexOf("+");
+        int sub = exp.indexOf("-");
+        int mul = exp.indexOf("*");
+        int div = exp.indexOf("/");
+        int exponent = exp.indexOf("^");
+        int mod = exp.indexOf("%");
+        double ans = 0.0;
+
+        // decides which operation
+        if (add != -1) {
+            ans = num1 + num2;
+        }
+        if (sub != -1) {
+            ans = num1 - num2;
+        }
+        if (mul != -1) {
+            ans = num1 * num2;
+        }
+        if (div != -1) {
+            ans = num1 / num2;
+        }
+        if (exponent != -1) {
+            ans = Math.pow(num1, num2);
+        }
+        if (mod != -1) {
+            ans = num1 % num2;
+        }
+
+        return ans;
+    }
 }
--- src/CalculatorRunner.java
+++ src/CalculatorRunner.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class CalculatorRunner {
+import java.util.Scanner;
+
+public class CalculatorRunner {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter an expression with *, /, +, -, %, or ^: ");
+        String input = scan.nextLine();
+        int space = input.indexOf(" ");
+        double num1 = Double.parseDouble(input.substring(0, space));
+        double num2 = Double.parseDouble(input.substring(space + 3)); // 2nd num is always 3 away
+        Calculator nums = new Calculator(input, num1, num2);
+
+        System.out.println("Result: " + nums.getAnswer());
+    }
 }
